6a9dfb9a340ee5b3e4df3a69b46b7f8810245247
link2mountain <linhtn@vnpay.vn>
2021-11-29 16:23:08 +0700

    update checked all

--- src/App.js
+++ src/App.js
@@ -7,55 +7,92 @@ import "./styles.css";
 import { components } from "react-select";
 
 const Option = props => {
-  return (
-      <div>
-        <components.Option {...props}>
-          <input
-              type="checkbox"
-              checked={props.isSelected}
-              onChange={() => null}
-          />{" "}
-          <label>{props.label}</label>
-        </components.Option>
-      </div>
-  );
+    return (
+        <div>
+            <components.Option {...props}>
+                <input
+                    type="checkbox"
+                    checked={props.isSelected}
+                    onChange={() => null}
+                />{" "}
+                <label>{props.label}</label>
+            </components.Option>
+        </div>
+    );
 };
 
-const MultiValue = props => (
-    <components.MultiValue {...props}>
-      <span>{props.data.label}</span>
-    </components.MultiValue>
-);
+const allOption = {
+    label: "Select all",
+    value: "*"
+};
+
+const ValueContainer = ({ children, ...props }) => {
+    const currentValues = props.getValue();
+    let toBeRendered = children;
+    if (currentValues.some(val => val.value === allOption.value)) {
+        toBeRendered = [[children[0][0]], children[1]];
+    }
+
+    return (
+        <components.ValueContainer {...props}>
+            {toBeRendered}
+        </components.ValueContainer>
+    );
+};
+
+const MultiValue = props => {
+    let labelToBeDisplayed = `${props.data.label}`;
+    if (props.data.value === allOption.value) {
+        labelToBeDisplayed = "All is selected";
+    }
+    return (
+        <components.MultiValue {...props}>
+            <span>{labelToBeDisplayed}</span>
+        </components.MultiValue>
+    );
+};
 
 const animatedComponents = makeAnimated();
 export default class Example extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      optionSelected: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            optionSelected: null
+        };
+    }
+
+    handleChange = selected => {
+        this.setState({
+            optionSelected: selected
+        });
     };
-  }
 
-  handleChange = selected => {
-    this.setState({
-      optionSelected: selected
-    });
-  };
-
-  render() {
-    return (
+    render() {
+        return (
+            <span
+                class="d-inline-block"
+                data-toggle="popover"
+                data-trigger="focus"
+                data-content="Please selecet account(s)"
+            >
         <MySelect
             options={colourOptions}
             isMulti
             closeMenuOnSelect={false}
             hideSelectedOptions={false}
-            components={{ Option, MultiValue, animatedComponents }}
+            components={{
+                Option,
+                MultiValue,
+                ValueContainer,
+                animatedComponents
+            }}
             onChange={this.handleChange}
             allowSelectAll={true}
             value={this.state.optionSelected}
         />
-    );
-  }
+      </span>
+        );
+    }
 }
 
 const rootElement = document.getElementById("root");
